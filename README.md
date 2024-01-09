49bf249d46b486d7c229aab1e70f40a70bc45c85
tony <chcjswo@gmail.com>
2024-01-10 00:54:27 +0900

    artillery 테스트 스크립트 추가 및 결과 파일 생성

---
+++ README.md
@@ -0,0 +1,10 @@
+## 다양한 스프링 부트 실습
+
+### artillery 실행
+`artillery run --output report.json test-config.yml`
+
+
+### artillery 실행 결과 html로 변환
+`artillery report report.json --output report.html`
+
+
--- src/main/java/com/mocadev/hellospring/controller/HelloController.java
+++ src/main/java/com/mocadev/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.mocadev.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by chcjswo on 2020-07-21(021)
@@ -17,6 +18,12 @@ public class HelloController {
 	public String hell(Model model) {
 		model.addAttribute("data", "hello!!");
 		return "hello";
+	}
+
+	@GetMapping("/hello2")
+	@ResponseBody
+	public String hello() {
+		return "hello~~";
 	}
 
 }
